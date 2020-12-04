440f0d7bef24fe604c1e1d734d9522b730adaae8
iicrazypanda <k.schipper@gmail.com>
2020-12-04 03:42:24 +0100

    database aangemaakt + user entiteit + admin dashboard + security controller en login

---
+++ src/Controller/Admin/DashboardController.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace App\Controller\Admin;
+
+use EasyCorp\Bundle\EasyAdminBundle\Config\Dashboard;
+use EasyCorp\Bundle\EasyAdminBundle\Config\MenuItem;
+use EasyCorp\Bundle\EasyAdminBundle\Config\UserMenu;
+use Symfony\Component\Security\Core\User\UserInterface;
+use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractDashboardController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use App\Entity\User;
+
+class DashboardController extends AbstractDashboardController
+{
+    /**
+     * @Route("/admin", name="admin")
+     */
+    public function index(): Response
+    {
+        return parent::index();
+    }
+
+    public function configureDashboard(): Dashboard
+    {
+        return Dashboard::new()
+            ->setTitle('Geba');
+    }
+
+    public function configureMenuItems(): iterable
+    {
+        yield MenuItem::linktoDashboard('Dashboard', 'fa fa-home');
+        yield MenuItem::linkToCrud('User', 'fas fa-list', User::class);
+        // yield MenuItem::linkToCrud('The Label', 'fas fa-list', EntityClass::class);
+    }
+
+    public function configureUserMenu(UserInterface $user): UserMenu
+    { 
+         // Usually it's better to call the parent method because that gives you a
+        // user menu with some menu items already created ("sign out", "exit impersonation", etc.)
+        // if you prefer to create the user menu from scratch, use: return UserMenu::new()->...
+        return parent::configureUserMenu($user)
+            // use the given $user object to get the user name
+            ->setName($user->getFullName())
+            // use this method if you don't want to display the name of the user
+            ->displayUserName(false)
+
+            // you can return an URL with the avatar image
+            ->setAvatarUrl('https://...')
+            ->setAvatarUrl($user->getProfileImageUrl())
+            // use this method if you don't want to display the user image
+            ->displayUserAvatar(false)
+            // you can also pass an email address to use gravatar's service
+            ->setGravatarEmail($user->getMainEmailAddress())
+
+            // you can use any type of menu item, except submenus
+            ->addMenuItems([
+                MenuItem::linkToRoute('My Profile', 'fa fa-id-card', '...', ['...' => '...']),
+                MenuItem::linkToRoute('Settings', 'fa fa-user-cog', '...', ['...' => '...']),
+                MenuItem::section(),
+                MenuItem::linkToLogout('Logout', 'fa fa-sign-out'),
+            ]);
+    }
+}
---
+++ src/Entity/User.php
@@ -0,0 +1,251 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\UserRepository;
+use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Security\Core\User\UserInterface;
+
+/**
+ * @ORM\Entity(repositoryClass=UserRepository::class)
+ * @ORM\Table(name="`user`")
+ */
+class User implements UserInterface
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=180, unique=true)
+     */
+    private $email;
+
+    /**
+     * @ORM\Column(type="json")
+     */
+    private $roles = [];
+
+    /**
+     * @var string The hashed password
+     * @ORM\Column(type="string")
+     */
+    private $password;
+
+      /**
+     * @ORM\Column(type="boolean")
+     */
+    private $is_verified;
+
+      /**
+     * @ORM\Column(type="integer", nullable=true)
+     */
+    private $persoonsnr;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $voornaam;
+
+    /**
+     * @ORM\Column(type="string", length=255, nullable=true)
+     */
+    private $tussenvoegsel;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $achternaam;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $adres;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $woonplaats;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $postcode;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getEmail(): ?string
+    {
+        return $this->email;
+    }
+
+    public function setEmail(string $email): self
+    {
+        $this->email = $email;
+
+        return $this;
+    }
+
+    /**
+     * A visual identifier that represents this user.
+     *
+     * @see UserInterface
+     */
+    public function getUsername(): string
+    {
+        return (string) $this->email;
+    }
+
+    /**
+     * @see UserInterface
+     */
+    public function getRoles(): array
+    {
+        $roles = $this->roles;
+        // guarantee every user at least has ROLE_USER
+        $roles[] = 'ROLE_USER';
+
+        return array_unique($roles);
+    }
+
+    public function setRoles(array $roles): self
+    {
+        $this->roles = $roles;
+
+        return $this;
+    }
+
+    /**
+     * @see UserInterface
+     */
+    public function getPassword(): string
+    {
+        return (string) $this->password;
+    }
+
+    public function setPassword(string $password): self
+    {
+        $this->password = $password;
+
+        return $this;
+    }
+
+    /**
+     * @see UserInterface
+     */
+    public function getSalt()
+    {
+        // not needed when using the "bcrypt" algorithm in security.yaml
+    }
+
+    /**
+     * @see UserInterface
+     */
+    public function eraseCredentials()
+    {
+        // If you store any temporary, sensitive data on the user, clear it here
+        // $this->plainPassword = null;
+    }
+
+    public function getIsVerified(): ?bool
+    {
+        return $this->is_verified;
+    }
+
+    public function setIsVerified(bool $is_verified): self
+    {
+        $this->is_verified = $is_verified;
+
+        return $this;
+    }
+
+    public function getPersoonsnr(): ?int
+    {
+        return $this->persoonsnr;
+    }
+
+    public function setPersoonsnr(?int $persoonsnr): self
+    {
+        $this->persoonsnr = $persoonsnr;
+
+        return $this;
+    }
+
+    public function getVoornaam(): ?string
+    {
+        return $this->voornaam;
+    }
+
+    public function setVoornaam(string $voornaam): self
+    {
+        $this->voornaam = $voornaam;
+
+        return $this;
+    }
+
+    public function getTussenvoegsel(): ?string
+    {
+        return $this->tussenvoegsel;
+    }
+
+    public function setTussenvoegsel(?string $tussenvoegsel): self
+    {
+        $this->tussenvoegsel = $tussenvoegsel;
+
+        return $this;
+    }
+
+    public function getAchternaam(): ?string
+    {
+        return $this->achternaam;
+    }
+
+    public function setAchternaam(string $achternaam): self
+    {
+        $this->achternaam = $achternaam;
+
+        return $this;
+    }
+
+    public function getAdres(): ?string
+    {
+        return $this->adres;
+    }
+
+    public function setAdres(string $adres): self
+    {
+        $this->adres = $adres;
+
+        return $this;
+    }
+
+    public function getWoonplaats(): ?string
+    {
+        return $this->woonplaats;
+    }
+
+    public function setWoonplaats(string $woonplaats): self
+    {
+        $this->woonplaats = $woonplaats;
+
+        return $this;
+    }
+
+    public function getPostcode(): ?string
+    {
+        return $this->postcode;
+    }
+
+    public function setPostcode(string $postcode): self
+    {
+        $this->postcode = $postcode;
+
+        return $this;
+    }
+}
